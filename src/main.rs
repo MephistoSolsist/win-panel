9a0dca1fbbe04294daedd097e580b0e80edd726a
LazuliKao <LazuliKao233@outlook.com>
2023-10-18 21:46:00 +0800

    move to api

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-extern crate winapi;
-fn get_drives() -> Vec<char> {
-    let mut result = vec![];
-    unsafe {
-        //获取所有逻辑驱动器
-        //u32，总共32位，每一位代表一个驱动器，1表示可用，0表示不可用
-        let mut drives = winapi::um::fileapi::GetLogicalDrives();
-        for drive_letter in b'A'..=b'Z' {
-            if (drives & 1) != 0 {
-                result.push(drive_letter as char);
-            }
-            drives >>= 1;
-        }
-    }
-    result
+mod api {
+    pub mod drive;
 }
-fn main() {}
+fn main() {
+    api::drive::get_drives();
+}
