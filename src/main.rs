24e6e52cead64326493e29a1ea953d409107f990
LazuliKao <LazuliKao233@outlook.com>
2023-10-18 21:41:12 +0800

    fn get_drives()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 extern crate winapi;
-
-fn main() {
+fn get_drives() -> Vec<char> {
+    let mut result = vec![];
     unsafe {
+        //获取所有逻辑驱动器
+        //u32，总共32位，每一位代表一个驱动器，1表示可用，0表示不可用
         let mut drives = winapi::um::fileapi::GetLogicalDrives();
         for drive_letter in b'A'..=b'Z' {
             if (drives & 1) != 0 {
-                println!("Drive {} is available.", drive_letter as char);
+                result.push(drive_letter as char);
             }
             drives >>= 1;
         }
     }
+    result
 }
+fn main() {}
