a8a0a58d582bd259857897d6256e6435afd32111
LazuliKao <LazuliKao233@outlook.com>
2023-10-18 21:37:18 +0800

    GetLogicalDrives

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate winapi;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let mut drives = winapi::um::fileapi::GetLogicalDrives();
+        for drive_letter in b'A'..=b'Z' {
+            if (drives & 1) != 0 {
+                println!("Drive {} is available.", drive_letter as char);
+            }
+            drives >>= 1;
+        }
+    }
 }
