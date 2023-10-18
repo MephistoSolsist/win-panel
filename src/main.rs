4c51ca6f8320844176486840a1bb072feb5fea2c
Mephisto <1443735959@qq.com>
2023-10-18 22:22:55 +0800

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,50 @@
 extern crate winapi;
+mod service;
+use axum::{
+    response::Json,
+    routing::get,
+    Router,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use service::get_drives;
 
-fn main() {
-    unsafe {
-        let mut drives = winapi::um::fileapi::GetLogicalDrives();
-        for drive_letter in b'A'..=b'Z' {
-            if (drives & 1) != 0 {
-                println!("Drive {} is available.", drive_letter as char);
-            }
-            drives >>= 1;
-        }
-    }
+
+#[derive(Serialize, Deserialize)]
+struct Drive {
+    name: String,
+    size: String,
+    free: String,
+    used_percent: f64,
+}
+
+
+async fn disk() -> Json<Value> {
+    get_drives();
+    let c = Drive {
+        name: String::from("C:"),
+        size: "100.00GB".to_string(),
+        free: "50.00".to_string(),
+        used_percent: 50.0,
+    };
+    let d = Drive {
+        name: String::from("D:"),
+        size: "120.00GB".to_string(),
+        free: "63.00GB".to_string(),
+        used_percent: 47.5,
+    };
+    let drive_list = [&c,&d];
+    Json(json!(drive_list))
+}
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/disk", get(disk));
+    let url = &"0.0.0.0:3000".parse().unwrap();
+    axum::Server::bind(url)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
