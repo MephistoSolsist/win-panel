691c3c8e7ea57f6ef026a504070cde32fcbbec16
LazuliKao <LazuliKao233@outlook.com>
2023-10-21 15:23:06 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use sysinfo::{ComponentExt, SystemExt};
 
 #[tokio::main]
 async fn main() {
+    let sys = sysinfo::System::new_all();
+    for c in sys.boot_time() {
+        println!("{:?}", c);
+    }
     let app = setup_router();
     let url = "0.0.0.0:12345";
     axum::Server::bind(&url.parse().unwrap())
--- src/service/cpu.rs
+++ src/service/cpu.rs
@@ -1,15 +1,9 @@
-use sysinfo::{ComponentExt, SystemExt};
-
 pub struct CpuInfo {}
 pub fn get_cpu_info() -> CpuInfo {
-    let sys = sysinfo::System::new_all();
-    for c in sys.components() {
-        println!("{:?}", c);
-    }
-    println!("total memory: {} bytes", sys.total_memory());
-    println!("used memory : {} bytes", sys.used_memory());
-    println!("total swap  : {} bytes", sys.total_swap());
-    println!("used swap   : {} bytes", sys.used_swap());
+    // println!("total memory: {} bytes", sys.total_memory());
+    // println!("used memory : {} bytes", sys.used_memory());
+    // println!("total swap  : {} bytes", sys.total_swap());
+    // println!("used swap   : {} bytes", sys.used_swap());
 
     unsafe {
         return CpuInfo {};
