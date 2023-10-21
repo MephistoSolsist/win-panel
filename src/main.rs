7b18e75444a1a9b10a96828ea5a4f6f518dd8f3d
LazuliKao <LazuliKao233@outlook.com>
2023-10-21 12:56:01 +0800

    port

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use router::setup_router;
 #[tokio::main]
 async fn main() {
     let app = setup_router();
-    let url = "0.0.0.0:3000";
+    let url = "0.0.0.0:12345";
     axum::Server::bind(&url.parse().unwrap())
         .serve(app.into_make_service())
         .await
