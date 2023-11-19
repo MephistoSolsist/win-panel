8fd8a32e35471b7fc2c236435aa2f17a9842ba55
LazuliKao <LazuliKao233@outlook.com>
2023-11-19 17:29:52 +0800

    fix ical return

--- src/router/web.rs
+++ src/router/web.rs
@@ -1,23 +1,14 @@
-use axum::{routing::get, Router,response::Json};
+use axum::{routing::get, Router};
 use reqwest::Client;
-use serde_json::{json, Value};
 
-async fn ical() -> Json<Value> {
+async fn ical() -> String {
     let ical_url = "https://p203-caldav.icloud.com.cn/published/2/MTE2MDg5MTA3NTIxMTYwOJ62pmQYH-orN1EZPCTNLzb42OJtGwf4PeI0ojg6fXjzh83-l1lgCdpnbRdCvNPzgjuJI3hmuh3AUfSvecozMv4";
 
     let client = Client::new();
-    let response = client
-        .get(ical_url)
-        .send()
-        .await
-        .unwrap();
+    let response = client.get(ical_url).send().await.unwrap();
 
-    let body = response
-        .text()
-        .await
-        .unwrap();
-    Json(json!(body))
-
+    let body = response.text().await.unwrap();
+    body
 }
 
 pub fn web_router_setup() -> Router {
